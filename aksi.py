03fcf0a86467deee5c85bc38a5af2381fbb6fd2d
Revichi <111824316+Revichi@users.noreply.github.com>
2022-12-13 13:40:51 +0700

    Update aksi.py

--- aksi.py
+++ aksi.py
@@ -6,11 +6,8 @@ def normalisasi(x):
     df = pd.DataFrame([x],columns=cols)
     data_test = pd.read_csv('DataMin/fix.csv')
     data_test = data_test.drop(data_test.columns[0],axis=1)
-    # data_test = data_test.drop(data_test.columns[7:13],axis=1)
     # memasukkan data kedalam data test
     data_test = data_test.append(df,ignore_index = True)
-    # print(data_test.columns)
-    # print(data_test)
     # return data_test yang sudah dinormalisasi
     return joblib.load('DataMin/model.sav').fit_transform(data_test)
 
